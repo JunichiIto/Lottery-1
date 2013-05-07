86ffe99b1f427016f9afbfe6d332e210391ab0ff
aq2bq <pememo@gmail.com>
2013-05-07 14:24:19 +0900

    beforeメソッドで書き直し

--- lottery_spec.rb
+++ lottery_spec.rb
@@ -7,16 +7,20 @@ require './lottery'
 
 describe Lottery do
 
+  before do
+    @lottery = Lottery::new 3
+    @lottery.add('Ieyasu',1)
+    @lottery.add('Hideyoshi', 2)
+    @lottery.add('Nobunaga',5)
+    @lottery.add('Yoritomo', 2)
+    @lottery.add('Kamatari', 10)
+  end
+
   context "応募者が当選者規定数より多い"
-    it "当選者が３人いる" do
-      lottery = Lottery::new 3
-      lottery.add('Ieyasu',1)
-      lottery.add('Hideyoshi', 2)
-      lottery.add('Nobunaga',5)
-      lottery.add('Yoritomo', 2)
-      lottery.add('Kamatari', 10)   
-      expect(lottery.winners.size).to eq 3
+    it "当選者が３人いる" do  
+      expect(@lottery.winners.size).to eq 3
     end
+  end  
 
   context "応募者が当選者規定数より少ない" do
     it "[]を返す" do
