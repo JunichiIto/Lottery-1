1d05d84b75aac7d93e2ce6819507a8386831794e
aq2bq <pememo@gmail.com>
2013-05-07 14:29:18 +0900

    helperメソッドにする

--- lottery_spec.rb
+++ lottery_spec.rb
@@ -7,18 +7,20 @@ require './lottery'
 
 describe Lottery do
 
-  before do
-    @lottery = Lottery::new 3
-    @lottery.add('Ieyasu',1)
-    @lottery.add('Hideyoshi', 2)
-    @lottery.add('Nobunaga',5)
-    @lottery.add('Yoritomo', 2)
-    @lottery.add('Kamatari', 10)
+  def new_lottery
+    lottery = Lottery::new 3
+    lottery.add('Ieyasu',1)
+    lottery.add('Hideyoshi', 2)
+    lottery.add('Nobunaga',5)
+    lottery.add('Yoritomo', 2)
+    lottery.add('Kamatari', 10)
+    lottery
   end
 
   context "応募者が当選者規定数より多い"
-    it "当選者が３人いる" do  
-      expect(@lottery.winners.size).to eq 3
+    it "当選者が３人いる" do
+      lottery = new_lottery
+      expect(lottery.winners.size).to eq 3
     end
   end  
 
